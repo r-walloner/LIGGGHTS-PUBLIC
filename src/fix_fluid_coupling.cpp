40ac584458409016b7c91725ece16d39ddba82d1
Robin Walloner <wallonerrobin@gmail.com>
2025-06-21 13:53:40 +0200

    Improve handling of drag and buoyancy

--- src/fix_fluid_coupling.cpp
+++ src/fix_fluid_coupling.cpp
@@ -416,13 +416,8 @@ void FixFluidCoupling::compute_force()
     // ---------------------------
     for (int d = 0; d < 3; d++)
     {
-      f_gravity[i][d] = gravity_flag
-                            ? volume[i] * atom->density[i] * gravity[d]
-                            : 0.0;
-
-      f_buoyancy[i][d] = buoyancy_flag
-                             ? volume[i] * rho_fluid * gravity[d]
-                             : 0.0;
+      f_gravity[i][d] = gravity_flag * volume[i] * atom->density[i] * gravity[d];
+      f_buoyancy[i][d] = buoyancy_flag * volume[i] * rho_fluid * gravity[d];
     }
 
     // apply forces to particle
